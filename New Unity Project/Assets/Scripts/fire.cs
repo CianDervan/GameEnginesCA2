ae96884ac194826882dea5321a05ba2559f8e461
CianDervan <44029453+CianDervan@users.noreply.github.com>
2020-05-17 13:18:43 +0100

    Fire,Target, Colliders

---
+++ New Unity Project/Assets/Scripts/fire.cs
@@ -0,0 +1,33 @@
+﻿
+using UnityEngine;
+
+public class fire : MonoBehaviour
+{
+    public float damage = 50f;
+    public float range = 200f;
+
+    public Camera TPS_Cam;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+       if(Physics.Raycast(TPS_Cam.transform.position, TPS_Cam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            target Target = hit.transform.GetComponent<target>();
+            if(Target != null)
+            {
+                Target.TakeDamage(damage);
+            }
+        }
+    }
+}
